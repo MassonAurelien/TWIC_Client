677549fb3264203ddffeca7ab980d4b3c8a066d1
beliaran <renan.malhere@reseau.eseo.fr>
2022-05-01 11:59:47 +0200

    affichage ville

--- src/main/java/com/twic_client/Servlet/ListeVilleServlet.java
+++ src/main/java/com/twic_client/Servlet/ListeVilleServlet.java
@@ -1,2 +1,74 @@
-package com.twic_client.Servlet;public class ListeVilleServlet {
+package com.twic_client.Servlet;
+
+import com.google.gson.Gson;
+import com.twic_client.Beans.VilleModel;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name="ListeVilleServlet", value="/ListeVille")
+public class ListeVilleServlet extends HttpServlet {
+
+    private int nbVilleAffiche;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        VilleModel[] villes = this.getVille("http://localhost:8181/ville");
+        HttpSession session = request.getSession();
+
+        List<VilleModel> villeAffiche = new ArrayList<>();
+        VilleModel ville = new VilleModel();
+
+        for(int i=0; i<nbVilleAffiche ; i++){
+            ville = villes[i];
+            villeAffiche.add(ville);
+        }
+        int numPage = 1;
+        request.setAttribute("villesAffichees", villeAffiche);
+        request.setAttribute("numPage",numPage);
+
+
+        this.getServletContext().getRequestDispatcher("/WEB-INF/ville.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        int numPage = Integer.parseInt(request.getParameter("numPage"));
+        request.setAttribute("numPage",numPage);
+
+        VilleModel[] villes = this.getVille("http://localhost:8181/ville");
+
+        int begin;
+        if(numPage == 1) begin = 0;
+        else begin = 50*(numPage-1);
+
+        List<VilleModel> villeAffiche = new ArrayList<>();
+        VilleModel ville = new VilleModel();
+
+        for(int i = begin; i<begin+50;i++) {
+            if (i < villes.length) {
+                ville = villes[i];
+                villeAffiche.add(ville);
+            }
+        }
+        request.setAttribute("villesAffichees", villeAffiche);
+
+        this.getServletContext().getRequestDispatcher("/WEB-INF/ville.jsp").forward(request, response);
+    }
+
+    public VilleModel[] getVille(String url) throws IOException {
+        JSONArray json = new JSONArray(IOUtils.toString(new URL(url), Charset.forName("UTF-8")));
+        VilleModel[] villes = new Gson().fromJson(json.toString(),VilleModel[].class);
+        return villes;
+    }
 }
--- src/main/webapp/WEB-INF/ville.jsp
+++ src/main/webapp/WEB-INF/ville.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: Renan
@@ -7,10 +8,52 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>Affiche ville</title>
+    <style>
+        table
+        {
+            border-collapse: collapse;
+        }
+        td, th /* Mettre une bordure sur les td ET les th */
+        {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <table>
+            <caption>List des villes</caption>
+
+            <tr>
+                <th>Code INSEE</th>
+                <th>Commune</th>
+                <th>Code Postal</th>
+                <th>Libelle Acheminement</th>
+                <th>Ligne 5</th>
+                <th>Longitude</th>
+                <th>Latitude</th>
+
+            </tr>
+            <c:forEach items="${villesAffichees}" var="ville">
+                <tr>
+                    <td>${ville.getCodeINSEE()}</td>
+                    <td>${ville.getCommune()}</td>
+                    <td>${ville.getCodePostal()}</td>
+                    <td>${ville.getLibelleAcheminement()}</td>
+                    <td>${ville.getLigne5()}</td>
+                    <td>${ville.getLongitude()}</td>
+                    <td>${ville.getLatitude()}</td>
+                </tr>
+            </c:forEach>
+        </table>
+
+        <form method = "post" action="ville">
+            <c:forEach var="i" begin="1" end="68">
+                <input type="submit" type="hidden" name="page" id="page" value="${i}">
+            </c:forEach>
+        </form>
+    </div>
+</body>
 </html>
